040c077a18819041863bec90dbbbe6be1df1446e
Kanata Miyahana <afiri.bupa@gmail.com>
2023-04-29 21:05:00 +0900

    add tmp test

--- mockloc_test.go
+++ mockloc_test.go
@@ -1,10 +1,23 @@
 package mockloc_test
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/gostaticanalysis/testutil"
+	"github.com/k3forx/mockloc"
+	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func TestAnalyzer(t *testing.T) {
-	// testdata := testutil.WithModules(t, analysistest.TestData(), nil)
-	// analysistest.Run(t, testdata, mockloc.Analyzer, "a")
+func TestAnalyzerRun(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	res := analysistest.Run(t, testdata, mockloc.Analyzer, "a")
+	fmt.Printf("res: %+v\n", res)
+	// cases := map[string]struct{
+
+	// }{}
+
+	// for name, c := range cases {
+
+	// }
 }
